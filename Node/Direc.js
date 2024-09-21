30b6f515f50672f33b2f4e6bcab0b410e5c2e8c6
nicxxxo <nicolashhperez@gmail.com>
2024-09-20 20:00:48 -0500

    Delete Node directory

--- Node/Direc.js
+++
@@ -1,6 +0,0 @@
-let fs=require('fs')
-fs.mkdirSync('Hoy')
-fs.mkdir('mañana',(err)=>{
-    if(err) 
-        throw err
-})	
--- Node/archivo.js
+++
@@ -1,5 +0,0 @@
-let fs=require('fs')
-
-// fs.renameSync('./textoarchivo.txt','./adso.txt')	
-// fs.renameSync('./adso.txt','./MoverArchivos/adso.txt')
-fs.unlinkSync('./MoverArchivos/adso.txt')
